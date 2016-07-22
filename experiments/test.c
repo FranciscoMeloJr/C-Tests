97eab52fbc1b66ad3e3884b6fa7af1aa72d3ac6a
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2016-07-22 15:21:01 -0400

    put the tracepoint

--- experiments/test.c
+++ experiments/test.c
@@ -16,17 +16,20 @@ void foo(){
 	printf("\n foo \n");
 	bar();
 }
-void demilimiter
+
 int main() {
 	/*My 239th program in C*/
 	/*printf("Hello, World! \n"); */
 	      
         for (int i = 0; i < 10; i++) {
                 // tracepoint interval_begin, foo_provider 
-                
+                tracepoint(hello_world, my_first_tracepoint, 23, "begin");              
+
 		foo();
         	bar();
 	        baz();
+
+                tracepoint(hello_world, my_first_tracepoint, 23, "end");
 
                 //tracepoint interval_end
 	}       
