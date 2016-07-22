41a49298d1e40ad5222055f50ffcfe8252d88786
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2016-07-22 18:08:05 -0400

    Changes on script_test related with -fInstrument

--- experiments/probe2/script_testF.sh
+++ experiments/probe2/script_testF.sh
@@ -4,17 +4,21 @@
 #gcc -o foo test.c -g -finstrument-functions
 #chmod +x foo
 nm testF > mapping.txt
+gcc -c -I. tp.c
+gcc -c test.c -finstrument-functions
+gcc -o testF test.o tp.o -llttng-ust -ldl
 
 lttng create testF
 lttng enable-event -u -a
 lttng add-context -u -t vpid -t vtid -t procname
 lttng start
 
-LD_PRELOAD=/usr/local/lib/liblttng-ust-cyg-profile.so ./testF
+LD_PRELOAD=liblttng-ust-cyg-profile.so ./testF > /dev/null
+#LD_PRELOAD=/usr/local/lib/liblttng-ust-cyg-profile.so ./testF
+#LD_PRELOAD=/usr/lib/x86_64-linux-gnu/liblttng-ust-cyg-profile.so ./testF > /dev/null
 
 lttng stop
 lttng destroy
 cd /home/frank/lttng-traces/
-clear
 
 ls
