d9e03a32f8d163ae04e912bf3b7ed2f5095e54d7
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2016-07-22 14:19:03 -0400

    tests for ECCT

---
+++ experiments/test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void baz(){
+	usleep(1000);
+        printf("\n baz \n");
+        
+}
+void bar(){
+	usleep(1000);
+        printf("\n bar \n");
+        baz();
+}
+void foo(){
+	usleep(1000);
+	printf("\n foo \n");
+	bar();
+}
+void demilimiter
+int main() {
+	/*My 239th program in C*/
+	/*printf("Hello, World! \n"); */
+	      
+        for (int i = 0; i < 10; i++) {
+                // tracepoint interval_begin, foo_provider 
+                
+		foo();
+        	bar();
+	        baz();
+
+                //tracepoint interval_end
+	}       
+
+	return 0;
+}
