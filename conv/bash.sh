5e84f1c29fc4d70959c6533849246e06689706c5
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2018-11-14 10:33:18 -0500

    Matrice multiplication, Eigen optimization/O2 opt.

---
+++ conv/bash.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ -O2 conv.cpp -o myConv
--- conv/conv.cpp
+++ conv/conv.cpp
@@ -4,26 +4,62 @@
 using namespace std;
 
 void convolution(int N[][WIDTH2], int M[][MASK_WIDTH2], int P[][WIDTH2]);
+void read_m(M[1024][1024]);
+
+#define H 1024;
+#define W 1024;
+#define W2 1024;
 
 int main()
 {
-    int H = 1024;
-    int W = 1024;
-
-    int A[H][W];
-    int B[H][W];
-    int C[H][W];
-   
-    read_a(A);
-    read_b(B);
     
-    convolutional(A,B,C);
+    
+    int A[H][W];
+    int B[W][W2];
+    int C[H][W2];
+   
+    read_m(A);
+    read_m(B);
+    
+    mult(A,B,C);
     
     return 0;
 }
 
+//Function to read matrix:
+void read{
+	ifstream f("matrix.txt");
+	f >> m >> n;
+	
+	if ((m != 4) || (n != 3))
+	  {
+	  cout << "Matrix not 4 by 3!n";
+	  return 1;
+	  }
+	
+	for (int i = 0; i < m; i++)
+	for (int j = 0; j < m; j++)
+	  f >> A[i][j];
+}
+
+void mult(A[H][W],B[W][W2],C[H][W2]){
+  // C = Ai,j,*Bj,k
+    for(i = 0; i < r1; ++i)
+        for(j = 0; j < c2; ++j)
+            for(k = 0; k < c1; ++k)
+            {
+            	//Possible optimization is the  
+                mult[i][j] += a[i][k] * b[k][j];
+            }
+}
+
+//Function based from [1].
+//[1] https://stackoverflow.com/questions/41452226/c-2d-convolution
+
 void convolution(int N[][WIDTH2], int M[][MASK_WIDTH2], int P[][WIDTH2]) {
-//N[WIDTH1][WIDTH2] is the input matrix, M[MASK_WIDTH1][MASK_WIDTH2] is the kernel matrix, P[][] the output matrix.
+//N[WIDTH1][WIDTH2] is the input matrix
+//M[MASK_WIDTH1][MASK_WIDTH2] is the kernel matrix
+//P[][] the output matrix.
 
 // find center position of kernel (half of kernel size)
 int kCenterX = MASK_WIDTH2 / 2;
@@ -53,3 +89,6 @@ for (int i = 0; i < WIDTH1; ++i)              // rows
     }
 }
 
+//Possible Optimization ways:
+//https://attractivechaos.wordpress.com/2016/08/28/optimizing-matrix-multiplication/
+//#include <Eigen/Dense> --> uses openmp has one of the best performance
